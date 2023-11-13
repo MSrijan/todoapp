9b3ea9ba4e651ba6528c9923be05ab1eb0b18ec1
Srijan <srijanm20@gmail.com>
2023-11-13 16:05:58 +0545

    Added multiple pages

--- lib/home.dart
+++ lib/home.dart
@@ -12,16 +12,64 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final todoList = ToDo.todoList();
   final todocontroller = TextEditingController();
-
+  int currentpageindex = 0;
 @override
 Widget build(BuildContext context) {
   return Scaffold(
     backgroundColor: Color.fromARGB(255, 235, 235, 235),
     appBar: buildAppbar(),
-    body: body(),
+    body:body[currentpageindex],
     floatingActionButton: actionbutton(),
     drawer: _drawer(),
+    bottomNavigationBar:navbar()
   );
+}
+
+NavigationBar navbar() {
+  return NavigationBar(
+    backgroundColor: Colors.white,
+    onDestinationSelected: (int index){
+      setState(() {
+        currentpageindex = index;
+      });
+    },
+    selectedIndex: currentpageindex,
+    destinations: [
+      homepage(),
+      editpage(),
+      accountpage(),
+    ],
+  );
+}
+
+List<Widget> get body {
+  return <Widget>[
+    Container(
+      margin: EdgeInsets.fromLTRB(20, 10, 20, 10), 
+      child: ListView(
+        children: [
+          Text(
+            "To Do List",
+            style: TextStyle(fontSize: 30),
+          ),
+          for (ToDo todoo in todoList)
+          Todocontainer(
+            todo: todoo,
+            ontodochanged: _handleChange,
+            ondelete: _deletetodo,
+            ),
+      ],
+      ),
+   ),
+   Container(
+    color: Colors.red,
+    child: Text("blue"),
+   ),
+   Container(
+    color: Colors.blue,
+    child: Text("Red"),
+   )
+];
 }
 
 Drawer _drawer() {
@@ -47,32 +95,11 @@ FloatingActionButton actionbutton() {
     }
     );
 }
-
-Container body() {
-  return Container(
-    margin: EdgeInsets.fromLTRB(20, 10, 20, 10), 
-    child: ListView(
-      children: [
-        Text(
-          "To Do List",
-          style: TextStyle(fontSize: 30),
-        ),
-        for (ToDo todoo in todoList)
-        Todocontainer(
-          todo: todoo,
-          ontodochanged: _handleChange,
-          ondelete: _deletetodo,
-          ),
-
-     ],
-    ),
-  );
-}
-  AppBar buildAppbar() {
+AppBar buildAppbar() {
     return AppBar(
       backgroundColor: Colors.indigo,
     );
-  }
+}
 
   Future openDialog() => showDialog(context: context,
   builder: (context) => AlertDialog(
@@ -114,5 +141,44 @@ void _addtodoitem(String toDo){
 }
 }
 
+class accountpage extends StatelessWidget {
+  const accountpage({
+    super.key,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDestination(
+      icon: Icon(Icons.account_circle_rounded),
+      label: "Account"
+      );
+  }
+}
 
+class editpage extends StatelessWidget {
+  const editpage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDestination(
+      icon: Icon(Icons.edit),
+      label: "Edit"
+      );
+  }
+}
+
+class homepage extends StatelessWidget {
+  const homepage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDestination(
+      icon: Icon(Icons.home),
+      label: "Home"
+      );
+  }
+}
